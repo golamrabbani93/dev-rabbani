a82d653a6ed7619fd00f4ba672e8ef5e0bae435d
golamrabbani93 <golamrabbani9373@gmail.com>
2023-02-05 15:11:34 +0600

    Add photo slider

--- src/pages/Homes/SingleProject/SingleProject.js
+++ src/pages/Homes/SingleProject/SingleProject.js
@@ -1,6 +1,8 @@
 import {useQuery} from '@tanstack/react-query';
 import React from 'react';
 import {useParams} from 'react-router-dom';
+import {PhotoProvider, PhotoView} from 'react-photo-view';
+import 'react-photo-view/dist/react-photo-view.css';
 import './SingleProject.scss';
 const SingleProject = () => {
 	const id = useParams();
@@ -21,11 +23,31 @@ const SingleProject = () => {
 			<div className="container ">
 				<h2 className="heading-primary--main text-white font-bebas mb-5">{name}</h2>
 				<div className="row gap-5 mx-auto text-center">
-					{site_images?.map((image) => (
+					{/* {site_images?.map((image) => (
 						<div className="project-image p-5 col-md-3 col-12">
 							<img src={image} alt="" width={`300px`} height={`auto`} />
+							<PhotoProvider>
+								<PhotoView src={image}>
+									<img src={image} alt="" width={`300px`} height={`auto`} />
+								</PhotoView>
+							</PhotoProvider>
 						</div>
-					))}
+					))} */}
+					<PhotoProvider>
+						{site_images.map((item, index) => (
+							<PhotoView key={index} src={item}>
+								{index < 3 ? (
+									<img
+										className="slide-img pb-5"
+										src={item}
+										alt=""
+										width={`300px`}
+										height={`auto`}
+									/>
+								) : undefined}
+							</PhotoView>
+						))}
+					</PhotoProvider>
 				</div>
 				<div className="project_details pb-5">
 					<h3 className="text-white fe">
