b54d6b966d58a11e07035f52be42280908fadf57
golamrabbani93 <golamrabbani9373@gmail.com>
2023-02-05 15:14:11 +0600

    change photo slider css

--- src/pages/Homes/SingleProject/SingleProject.js
+++ src/pages/Homes/SingleProject/SingleProject.js
@@ -23,16 +23,6 @@ const SingleProject = () => {
 			<div className="container ">
 				<h2 className="heading-primary--main text-white font-bebas mb-5">{name}</h2>
 				<div className="row gap-5 mx-auto text-center">
-					{/* {site_images?.map((image) => (
-						<div className="project-image p-5 col-md-3 col-12">
-							<img src={image} alt="" width={`300px`} height={`auto`} />
-							<PhotoProvider>
-								<PhotoView src={image}>
-									<img src={image} alt="" width={`300px`} height={`auto`} />
-								</PhotoView>
-							</PhotoProvider>
-						</div>
-					))} */}
 					<PhotoProvider>
 						{site_images.map((item, index) => (
 							<PhotoView key={index} src={item}>
