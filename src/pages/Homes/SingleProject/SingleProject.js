08fa051c8ef6ad2e750bccfdba81304ce425d339
golamrabbani93 <golamrabbani9373@gmail.com>
2023-09-27 20:04:40 +0600

    single portfolio update

--- src/pages/Homes/SingleProject/SingleProject.js
+++ src/pages/Homes/SingleProject/SingleProject.js
@@ -21,13 +21,13 @@ const SingleProject = () => {
 	if (isLoading) {
 		return <h2>loading..........</h2>;
 	}
-	const {feature, name, site_images, technology} = project;
+	const {name, site_images, technology} = project;
 	return (
 		<div>
-			<div className="project position-absolute top-0 ">
+			<div className="project position-absolute ">
 				<div className="container ">
 					<h2 className="heading-primary--main text-white font-bebas mb-5">{name}</h2>
-					<div className="row gap-5 mx-auto text-center">
+					<div className="row gap-5 mx-auto justify-content-center text-center mt-5">
 						<PhotoProvider>
 							{site_images.map((item, index) => (
 								<PhotoView key={index} src={item}>
@@ -44,19 +44,24 @@ const SingleProject = () => {
 							))}
 						</PhotoProvider>
 					</div>
-					<div className="project_details pb-5">
-						<h3 className="text-white fe">
-							<span className="me-4" style={{color: '#D94D3A'}}>
-								Technology Used:
-							</span>
-							{technology}
-						</h3>
-						<h3 className="text-white fe">
-							<span className="me-4" style={{color: '#D94D3A'}}>
-								Feature:
-							</span>
-							{feature}
-						</h3>
+					<div className="project_details py-5">
+						<h2 className="text-white">Technology Used In This Project</h2>
+
+						<div className="technology row ">
+							{technology?.map((item) => (
+								<div class="card justify-content-center align-items-center flex-column">
+									{item?.image?.length > 200 ? (
+										<img alt="" src={`data:image/svg+xml;utf8,${encodeURIComponent(item.image)}`} />
+									) : (
+										<img alt="" src={item.image} />
+									)}
+
+									<div class="card-body">
+										<h5 class="card-title">{item.name}</h5>
+									</div>
+								</div>
+							))}
+						</div>
 					</div>
 				</div>
 			</div>
