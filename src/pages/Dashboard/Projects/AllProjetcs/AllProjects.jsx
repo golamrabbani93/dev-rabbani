c7a7209cd565a22684430f5a3c3434727e6985f0
Golam Rabbani <golamrabbani9373@gmail.com>
2024-10-22 21:36:04 +0600

    show all project complete in dashboard

--- src/pages/Dashboard/Projects/AllProjetcs/AllProjects.jsx
+++ src/pages/Dashboard/Projects/AllProjetcs/AllProjects.jsx
@@ -1,40 +1,85 @@
-import {Table} from 'antd';
+import {Table, Tag} from 'antd';
+import {useGetAllProjectQuery} from '../../../../redux/features/project/project.management';
 
 const AllProjects = () => {
-	const dataSource = [
-		{
-			key: '1',
-			name: 'Mike',
-			age: 32,
-			address: '10 Downing Street',
-		},
-		{
-			key: '2',
-			name: 'John',
-			age: 42,
-			address: '10 Downing Street',
-		},
-	];
+	const {data: AllProjects, isLoading, isError} = useGetAllProjectQuery();
+	const projectData = AllProjects?.data;
+
+	const newDataSource = projectData?.map((item) => {
+		return {
+			key: item._id,
+			name: item.name,
+			photo: item.fullPhoto,
+			liveLink: item.liveLink,
+			status: item.status,
+		};
+	});
+
+	console.log('🚀🚀: AllProjects -> newDataSource', newDataSource);
+	// const dataSource = [
+	// 	{
+	// 		key: '1',
+	// 		name: 'Mike',
+	// 		age: 32,
+	// 		address: '10 Downing Street',
+	// 	},
+	// 	{
+	// 		key: '1',
+	// 		name: 'Mike',
+	// 		age: 32,
+	// 		address: '10 Downing Street',
+	// 	},
+	// 	{
+	// 		key: '2',
+	// 		name: 'John',
+	// 		age: 42,
+	// 		address: '10 Downing Street',
+	// 	},
+	// ];
 
 	const columns = [
 		{
-			title: 'Name',
+			title: 'Photo',
+			dataIndex: 'photo',
+			key: 'photo',
+			render: (text, record) => (
+				<img src={record.photo} alt="project" style={{width: '50px', height: '50px'}} />
+			),
+		},
+		{
+			title: 'Project Name',
 			dataIndex: 'name',
 			key: 'name',
 		},
+
 		{
-			title: 'Age',
-			dataIndex: 'age',
-			key: 'age',
+			title: 'Live Link',
+			dataIndex: 'liveLink',
+			key: '  liveLink',
+			render: (text, record) => (
+				<a href={record.liveLink} target="_blank" rel="noreferrer">
+					Live Link
+				</a>
+			),
 		},
 		{
-			title: 'Address',
-			dataIndex: 'address',
-			key: 'address',
+			title: 'Project Status',
+			dataIndex: 'status',
+			key: 'status',
+			render: (text, record) => (
+				<Tag
+					color="success"
+					style={{
+						textTransform: 'uppercase',
+					}}
+				>
+					{text}
+				</Tag>
+			),
 		},
 	];
 
-	return <Table dataSource={dataSource} columns={columns} />;
+	return <Table loading={isLoading} dataSource={newDataSource} columns={columns} />;
 };
 
 export default AllProjects;
